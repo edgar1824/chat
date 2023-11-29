db7cc153c2ffd408692abd8ae8fcd29d79bcccbc
Edgar <edgaravetisyan932@gmail.com>
2023-11-29 15:40:33 +0400

    4: 29-11-2023 changes

--- socket/dist/events/index.js
+++
@@ -1,5 +0,0 @@
-export { userEvents } from "./users.js";
-export { messageEvents } from "./messages.js";
-export { conversationEvents } from "./conversations.js";
-export { notificationEvents } from "./notifications.js";
-//# sourceMappingURL=index.js.map
--- socket/dist/services/index.js
+++
@@ -1,2 +0,0 @@
-export { UserStore } from "./UserStore.js";
-//# sourceMappingURL=index.js.map
